ead3bf5cc7f7dc2f4be04375e6a646d4259f57ad
vietvmq <vietvmq.dev@gmail.com>
2023-09-02 15:27:56 +0700

    update: example

--- example/main.dart
+++ example/main.dart
@@ -16,14 +16,14 @@ void main() {
   final parser = FlutterJsonPath();
 
   var json = parser.gen('foo/bar/baz');
-  print('gen: $json');
+  print('gen: $json'); // gen: {foo: {bar: {baz: {}}}}
 
   var json1 = {...target};
   var value = parser.get(json1, 'foo/bar/baz');
   print('get: $value'); // get: 42
 
   var json2 = {...target};
-  parser.set(json2, 'foo/bar', null);
+  parser.set(json2, 'foo/bar', 'CLUB');
   print('set: $json2'); // set: {foo: {bar: CLUB}}
 
   var json3 = {...target};
